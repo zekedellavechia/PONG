e99de8c0e54e84d242168d42b9d31db1bf8b7c2d
Zeke Dellavechia <67669609+zekedellavechia@users.noreply.github.com>
2020-09-13 04:06:57 -0300

    fs

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //para editar la velocidad
+    public float speed;
+
+    //darle direcicion x e y
+    Vector2 direction;
+
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //referencia al Rigid
+        rb = GetComponent<Rigidbody2D>();
+
+        //que la direccion sea igual al vector2 (o sea 1,1 y normalizar el vector)
+        direction = Vector2.one.normalized;
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.velocity = direction * speed;
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction.y = -direction.y;
+        }
+        else if(collision.gameObject.CompareTag("Paddle"))
+        {
+            speed += Random.Range(0.5f, 1.5f);
+            direction.x = -direction.x;
+
+        }    
+    }
+
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    public float minY;
+    public float maxY;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ver si somos derecha o izquierda
+
+        if(gameObject.name=="RightPlayer")
+        {
+            MovePaddle(gameObject.name);
+
+        }   
+        
+        else if(gameObject.name=="LeftPlayer")
+        {
+            MovePaddle(gameObject.name);
+        }    
+
+        //mover el objeto
+        
+    }//update
+
+
+
+    void MovePaddle(string paddleName)
+    {
+        float moveY = Input.GetAxisRaw(paddleName) * speed * Time.deltaTime;
+        transform.position = new Vector2(
+         transform.position.x,
+         Mathf.Clamp(transform.position.y + moveY, minY, maxY));
+    }    
+}//class
